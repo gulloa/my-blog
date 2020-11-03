96a32def0562119f1cd4c882a2b9b1ef3e1c340c
gulloa <gulloa.uidev@gmail.com>
2020-11-02 23:36:00 -0600

    First commit.

---
+++ src/build/precache-manifest.ca3b6709f954dc3be8d2d4656e2fba46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8effd8118b51f6d2172",
+    "url": "/static/css/main.4eb27601.chunk.css"
+  },
+  {
+    "revision": "f8effd8118b51f6d2172",
+    "url": "/static/js/main.28f6a77e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "64162f3a8ef39f23f1ad",
+    "url": "/static/js/2.226fd69f.chunk.js"
+  },
+  {
+    "revision": "251051d04ce99e104f9f86ab6f88f70b",
+    "url": "/index.html"
+  }
+];
